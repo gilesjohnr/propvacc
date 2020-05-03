be5582b5d4125078b465680757c085ccfc6d0c39
gilesjohnr <gilesjohnr@gmail.com>
2020-05-03 13:54:25 -0400

    updating imports

---
+++ R/imports.R
@@ -0,0 +1,10 @@
+##' @import stats
+##' @import graphics
+##' @import grDevices
+##' @import utils
+##' @import methods
+##' @importFrom foreach foreach
+##' @importFrom foreach %do%
+##' @importFrom MASS fitdistr
+##' @importFrom mgcv gam
+NULL
